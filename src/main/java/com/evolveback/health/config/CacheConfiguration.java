95fd2cd54ebe1f76d37d5a76588d821182b31f83
Shwetha Manjunath <shweta.manjunath@orangecounty.in>
2017-10-11 13:08:08 +0530

    Added the entity Weight and its implementations.

--- src/main/java/com/evolveback/health/config/CacheConfiguration.java
+++ src/main/java/com/evolveback/health/config/CacheConfiguration.java
@@ -43,6 +43,7 @@ public class CacheConfiguration {
             cm.createCache(com.evolveback.health.domain.User.class.getName() + ".authorities", jcacheConfiguration);
             cm.createCache(com.evolveback.health.domain.Points.class.getName(), jcacheConfiguration);
             cm.createCache(com.evolveback.health.domain.BloodPressure.class.getName(), jcacheConfiguration);
+            cm.createCache(com.evolveback.health.domain.Weight.class.getName(), jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
