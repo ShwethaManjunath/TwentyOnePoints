7e29ecb77f321a3b0be5cc6424dd9d6332cbd084
Shwetha Manjunath <shweta.manjunath@orangecounty.in>
2017-10-11 12:55:47 +0530

    Added the entity Points and its implementations.

---
+++ src/main/java/com/evolveback/health/repository/search/PointsSearchRepository.java
@@ -0,0 +1,10 @@
+package com.evolveback.health.repository.search;
+
+import com.evolveback.health.domain.Points;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the Points entity.
+ */
+public interface PointsSearchRepository extends ElasticsearchRepository<Points, Long> {
+}
